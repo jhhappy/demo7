719661f5d6aeedc2ba43f2856aa039d0716519c6
jjh <jh18438000868@qq.com>
2023-05-18 15:03:04 +0800

    1111

---
+++ src/main/java/com/example/demo/SpringDataRedisTest.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ustinain
+ * @Time 2023/5/18  14:34
+ */
+@RestController
+public class SpringDataRedisTest {
+}
